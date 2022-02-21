5311b92ec4add87c5b4207515b552bdf5141c1d4
faisal2410 <faisal2410@yahoo.com>
2022-02-21 22:26:48 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import those pages in App.js
+// then based on the path show each components using react-router components
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./booking/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import TopNav from "./components/TopNav";
 
+/**
+ * Lets create TopNavigation/menu bar so that we can easily TopNavigate between pages
+ * lets write it in App.js before we move it to its own component
+ */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <TopNav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/booking/Home.js
@@ -0,0 +1,6 @@
+const Home = () => {
+    return <div className="container-fluid h1 p-5 text-center">Home Page</div>;
+  };
+  
+  export default Home;
+  
